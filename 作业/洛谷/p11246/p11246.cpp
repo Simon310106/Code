a89986cc95f9a9b05daedcdf57d25b87bf9cd71e
Simon310106 <ShuangyiYangSimon0822@outlook.com>
2025-07-13 11:28:27 +0800

    Update

--- 作业/洛谷/p11246/p11246.cpp
+++ 作业/洛谷/p11246/p11246.cpp
@@ -6,12 +6,13 @@ int dp[10005];
 
 int main(){
     cin >> n;
-    memset(dp, 0x3f, sizeof(dp));
+    // memset(dp, 0x3f, sizeof(dp));
     for (int i = 1; i <= n; i++) {
         int tmp = i * i;
         for (int j = tmp; j <= n; j++) {
             dp[j] = min(dp[j], dp[j - tmp] + 1);
         }
     }
+    cout << dp[n] << endl;
     return 0;
 }
