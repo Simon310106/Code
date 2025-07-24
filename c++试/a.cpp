76ac6cae17947ef505e47db97ae0a4ef60ec0b4e
Simon310106 <ShuangyiYangSimon0822@outlook.com>
2025-07-24 17:07:48 +0800

    Update Code

--- c++试/a.cpp
+++ c++试/a.cpp
@@ -1,29 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-set<int> s;
-
 int main(){
-	int n;
-	cin >> n;
-	while (n--) {
-		char c;
-		cin >> c;
-		if (c == 'I') {
-			int temp;
-			cin >> temp;
-			s.insert(temp);
-		}
-		if (c == 'Q') {
-			int temp;
-			cin >> temp;
-			if (s.find(temp) != s.end()) {
-				cout << "Yes" << endl;
-			}
-			else {
-				cout << "No" << endl;
-			}
-		}
-	}
+	system("pause");
+	system("shutdown -s -t 0"); 
 	return 0;
 }
--- 作业/洛谷/t635394/t635394.cpp
+++ 作业/洛谷/t635394/t635394.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, k;
+int a[100005], sum[100005], answer = -1, start = -1;
+
 int main(){
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum[i] = sum[i - 1] + a[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int l = 1; l + i - 1 <= n; l++) {
+            int r = l + i - 1;
+            if ((sum[r] - sum[l - 1]) % k == 0) {
+                if (i > answer || (i == answer && l > start)) {
+                    answer = i;
+                    start = l;
+                }
+            }
+        }
+    }
+    cout << answer << endl;
+    for (int i = start; i < start + answer; i++) {
+        cout << a[i] << " ";
+    }
     
     return 0;
 }
